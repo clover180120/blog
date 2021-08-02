b166c62b8917657d5ddf9e61c629f71d381e6ef6
Memo Chou <memochou1993@gmail.com>
2021-08-02 20:55:28 +0800

    Added fake comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 use App\Models\User;
@@ -15,8 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->count(5)
-            ->has(Post::factory()->count(3))
-            ->create();
+        $users = User::factory()->count(5)->create();
+
+        $users->each(function ($user) {
+            $posts = Post::factory()->count(10)->create([
+                'user_id' => $user->id,
+            ]);
+
+            $posts->each(function ($post) use ($user) {
+                Comment::factory()->count(3)->create([
+                    'user_id' => $user->id,
+                    'post_id' => $post->id,
+                ]);
+            });
+        });
     }
 }
